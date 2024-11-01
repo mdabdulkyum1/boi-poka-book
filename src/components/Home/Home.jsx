7a2c03894bde96766dba6ad07e607aca1ba23a22
mdabdulkyum1 <kyummdabdul@gmail.com>
2024-11-01 16:47:12 +0600

    banner and font added done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import bookImg from '../../assets/books.jpg'
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="hero bg-base-200 py-24 rounded-2xl">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img
+          src={bookImg}
+          className="max-w-sm rounded-lg shadow-2xl"
+        />
+        <div>
+          <h1 className="text-6xl font-bold">Books to freshen up <br /> your bookshelf</h1>
+          <button className="btn btn-success text-white mt-12">View The List</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
